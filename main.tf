90af1c52e7047e175716df76c976825200e3c5e1
Setra Wicana <gw.setra@gmail.com>
2024-05-25 21:00:49 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  profile = "setra-sso-profile"
+}
+
+resource "aws_s3_dsdbucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-paset-12399" # Replace with your desired bucket name
+  acl    = "private" # Sets the ACL to private (default is private if not specified)
+  
+  tags = {
+    Name        = "MyS3Bucket"
+    Environment = "Dev"
+  }
+}
